be6564a61620727ed386738ff5e866161c6904c4
sevengreenz <banchikichi+sevengreenz@gmail.com>
2019-04-03 00:24:20 +0900

    make it possible to use absolute pass

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 const withTypescript = require('@zeit/next-typescript')
 
 const config = {
-  distDir: '../dist'
+  distDir: '../dist',
+  webpack(config, options) {
+    config.resolve.modules.unshift(__dirname);
+    return config;
+  }
 }
 
 module.exports = withTypescript(config)
