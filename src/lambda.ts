9c604a52da5992e74ce9efa65978a4df2ea40a51
sevengreenz <banchikichi+sevengreenz@gmail.com>
2019-11-12 00:27:23 +0900

    add base64 encode to lambda handler becouse gzip compression by next

--- src/lambda.ts
+++ src/lambda.ts
@@ -2,7 +2,27 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import * as awsServerlessExpress from 'aws-serverless-express';
 import app from './app';
 
-const server = awsServerlessExpress.createServer(app);
+const binaryMimeTypes = [
+  'application/javascript',
+  'application/json',
+  'application/octet-stream',
+  'application/xml',
+  'font/eot',
+  'font/opentype',
+  'font/otf',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
+  'text/comma-separated-values',
+  'text/css',
+  'text/html',
+  'text/javascript',
+  'text/plain',
+  'text/text',
+  'text/xml'
+];
+
+const server = awsServerlessExpress.createServer(app, undefined, binaryMimeTypes);
 
 const handler: APIGatewayProxyHandler = async (event, context) => {
   return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
